039d4e21ed9d86fcfcaf71fdd0761a5fce41645c
Soundarya <67725756+soundarya-karli@users.noreply.github.com>
2023-10-26 15:56:01 +0000

    initial

---
+++ cruddemo/src/test/java/com/springboot/learning/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.learning.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
